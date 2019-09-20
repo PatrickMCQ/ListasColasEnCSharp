de3e454bb36c116960577b2efb4c615c0716b26c
PatrickMCQ <patrickmcq7@gmail.com>
2019-09-20 00:27:37 -0600

    Colas en C#

---
+++ ProgramacionColas/ProgramacionColas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionColas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            misColas nuevaCola = new misColas();
+            nuevaCola.agregarEnCola("elemento 01");
+            nuevaCola.agregarEnCola("elemento 02");
+            nuevaCola.agregarEnCola("elemento 03");
+            nuevaCola.agregarEnCola("elemento 04");
+            nuevaCola.imprimir();
+            nuevaCola.obtenerElementosExtraidosDeCola();
+            nuevaCola.imprimir();
+            nuevaCola.obtenerElementosSinSerExtraidosDeCola();
+            nuevaCola.imprimir();
+        }
+    }
+}
---
+++ ProgramacionColas/ProgramacionColas/misColas.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramacionColas
+{
+    class misColas
+    {
+        //Colas
+        Queue<string> cola = new Queue<string>();
+
+        public void agregarEnCola(string data)
+        {
+            cola.Enqueue(data);  //Estamos agregando un elemento a la cola, Enqueue es para insertar.
+        }
+
+        public void imprimir()
+        {
+            foreach(var item in cola)  //Este recorre toda la cola.
+            {
+                Console.WriteLine(item);  
+            }
+            Console.ReadKey();
+
+            
+        }
+
+        public void obtenerElementosExtraidosDeCola()
+        {
+            var elementoEstraido2 = cola.Dequeue();  //Dequeue es para extraer.
+            Console.WriteLine("Elemento extraido: " + elementoEstraido2);
+            Console.ReadKey();
+        }
+
+        public void obtenerElementosSinSerExtraidosDeCola()
+        {
+            var elementoEstraido2 = cola.Peek();  //Solo se consulta quien será atendido.
+            Console.WriteLine("Primer elmento: " + elementoEstraido2);
+            Console.ReadKey();
+        }
+    }
+}
